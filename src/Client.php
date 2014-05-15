25fbe9766ca765d928cff1b9a552a25c91fb1724
Corey Hinshaw <hinshaw.25@osu.edu>
2014-05-14 22:41:26 -0400

    Added overrides to handle additional SOAP headers

--- src/Client.php
+++ src/Client.php
@@ -9,9 +9,11 @@
 namespace WsSoap;
 
 class Client extends \SoapClient {
-    private $wsUsername;
-    private $wsPassword;
-    private $wsse;
+    protected $wsUsername;
+    protected $wsPassword;
+    protected $wsse;
+    protected $wsHeader;
+
 
     public function __construct($wsdl, array $options) {
         if (empty($options['wsUsername']) || empty($options['wsPassword']))
@@ -22,10 +24,22 @@ class Client extends \SoapClient {
         $this->wsse = empty($options['wsse']) ? 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd' : $options['wsse'];
 
         parent::__construct($wsdl, $options);
-        $this->addWsHeader();
+
+        $this->generateWsHeader();
+        $this->__setSoapHeaders();
     }
 
-    private function addWsHeader() {
+    public function __setSoapHeaders($soapheaders=null) {
+        parent::__setSoapHeaders();
+        return parent::__setSoapHeaders($this->addWsHeader($soapheaders));
+    }
+
+    public function __soapCall($function_name, $arguments, $options=null, $input_headers=null, &$output_headers=null) {
+        return parent::__soapCall($function_name, $arguments, $options, $this->addWsHeader($input_headers), $output_headers);
+    }
+
+
+    protected function generateWsHeader() {
         $soapVarUser = new \SoapVar($this->wsUsername, XSD_STRING, null, $this->wsse, null, $this->wsse);
         $soapVarPass = new \SoapVar($this->wsPassword, XSD_STRING, null, $this->wsse, null, $this->wsse);
 
@@ -35,8 +49,18 @@ class Client extends \SoapClient {
         $soapVarWsseToken = new \SoapVar($wsseToken, SOAP_ENC_OBJECT, null, $this->wsse, 'UsernameToken', $this->wsse);
         $soapVarHeaderVal = new \SoapVar($soapVarWsseToken, SOAP_ENC_OBJECT, null, $this->wsse, 'Security', $this->wsse);
 
-        $wsHeader = new \SoapHeader($this->wsse, 'Security', $soapVarHeaderVal, true);
-        $this->__setSoapHeaders(array($wsHeader));
+        $this->wsHeader = new \SoapHeader($this->wsse, 'Security', $soapVarHeaderVal, true);
+    }
+
+    protected function addWsHeader($headers=null) {
+        if (empty($headers)) {
+            $headers = array();
+        } elseif (!is_array($headers)) {
+            $headers = array($headers);
+        }
+
+        $headers[] = $this->wsHeader;
+        return $headers;
     }
 }
 
